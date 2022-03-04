6379d4a073babed7e6dfb015033e864c5ed19090
SNikhill <snikhill@protonmail.com>
2022-03-04 17:16:10 +0530

    chore(terminal): add fish config

---
+++ fish/conf.d/git.fish
@@ -0,0 +1,21 @@
+# Alias for FAST git usage.
+# Contains alias for common git commands I use
+
+alias ga="git add -A"
+alias gc="git commit"
+alias gc!="git commit --amend"
+
+alias gb="git branch"
+alias gl="git log"
+alias gs="git status"
+
+alias gR="git restore"
+alias gre="git restore"
+
+alias gp="git push"
+alias gf="git fetch"
+
+alias gr="git rebase"
+alias grb="git rebase"
+
+alias gch="git checkout"
