df078036011ead34d79a5be4b60bf62ede114711
SNikhill <snikhill@protonmail.com>
2022-03-09 02:17:57 +0530

    chore(nvim): store plugin git-urls

---
+++ nvim/plugins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export plugins=("https://github.com/Shougo/deoplete.nvim"
+	"https://github.com/preservim/nerdtree.git"
+	"https://github.com/airblade/vim-gitgutter.git"
+"https://github.com/vim-airline/vim-airline.git"
+"https://github.com/tomtom/tcomment_vim.git"
+"https://github.com/nvim-lua/plenary.nvim.git"
+"https://github.com/nvim-telescope/telescope.nvim.git"
+"https://github.com/ludovicchabant/vim-gutentags.git"
+"https://github.com/dense-analysis/ale.git"
+)
